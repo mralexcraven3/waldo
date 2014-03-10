a738ebad58636e732d5d33bf49b22bccfc063528
Omar Bohsali <omar.bohsali@gmail.com>
2014-03-10 16:49:58 -0700

    Bugfixes

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ import os
 import datetime as D
 import ujson as json
 from smarthat import SmartHat
-
+import time
 import tornado.options
 import tornado.httpclient
 from tornado.options import define, options, parse_command_line
@@ -21,14 +21,15 @@ pth = lambda x: os.path.join(os.path.dirname(__file__), x)
 
 
 define("debug", default=False, type=bool, help="debug mode?")
-define("port", default=1234, type=int, help="which port?")
-define("user_agents", default="user_agents.txt", type=str, 
-        help="list of user agents.")
+define("port", default=12345, type=int, help="which port?")
+define("user_agents", default="user_agents.txt", type=str,
+       help="list of user agents.")
 define("loglevel", default='INFO', type=str, help='logging level')
+define("warmup", default=False, type=bool, help="warmup server?")
 
 
 def parse_proxy(p):
-    """Parse a proxy into its component parts. 
+    """Parse a proxy into its component parts.
 
     This should eventually use httplib to support usernames and passwords.
 
@@ -41,8 +42,8 @@ def parse_proxy(p):
     _p = p.split(':')
     # curl_httpclient requires proxy_host to be a string
     return dict(
-        proxy_host = str(_p[0]),
-        proxy_port = int(_p[1])
+        proxy_host=str(_p[0]),
+        proxy_port=int(_p[1])
     )
 
 
@@ -64,26 +65,39 @@ class ProxyServer(HTTPServer):
     successes = 0
     failures = 0
 
-
     def __init__(self, *args, **kwargs):
         self.application = None
         self.user_agents = self._get_user_agents()
-        self.proxies = SmartHat(self._get_proxy_list())
+        self.refresh_proxies()
         super(ProxyServer, self).__init__(self.handle_request, **kwargs)
 
     def _get_user_agents(self, fname=pth("user_agents.txt")):
         with open(fname, 'r') as f:
             return f.readlines()
 
-    def _get_proxy_list(self, source_url=source_url):
-        response = self.blocking_http_client.fetch(source_url)
-        self.last_proxy_update = D.datetime.now()
-        return map(parse_proxy, json.loads(response.body)['values'])
+    def refresh_proxies(self, source_url=source_url):
+        logging.info("Fetching proxy list")
+        while 1:
+            try:
+                response = self.blocking_http_client.fetch(source_url)
+                logging.info("Fetched proxy list.")
+                values = map(parse_proxy, json.loads(response.body)['values'])
+            except Exception, e:
+                print "Exception ", e
+                time.sleep(10)
+            else:
+                self.proxies = SmartHat(values)
+                self.last_proxy_update = D.datetime.now()
+                break
+
+    def needs_proxy_refresh(self):
+        return D.datetime.now() > (self.last_proxy_update +
+                D.timedelta(seconds=86400))
 
     def get_proxy(self):
-        if D.datetime.now() > self.last_proxy_update + D.timedelta(hours=24):
-            logging.debug("Update proxy list.")
-            self.proxies = self._update_proxy_list()
+        if self.needs_proxy_refresh():
+            print "Refresh proxies"
+            self.refresh_proxies()
         return self.proxies.pop()
 
     def feign_user_agent(self):
@@ -122,10 +136,10 @@ class ProxyServer(HTTPServer):
             try:
                 proxy = self.get_proxy()
                 response = yield self.http_client.fetch(request.uri,
-                        headers=request.headers, 
-                        request_timeout=request_timeout,
-                        **proxy.obj)
-            except (Exception, tornado.httpclient.HTTPError) as e:
+                                                        headers=request.headers,
+                                                        request_timeout=request_timeout,
+                                                        **proxy.obj)
+            except tornado.httpclient.HTTPError as e:
                 logging.error(e)
                 if e.code in (400, 404, 599):
                     # wtf pycurl
@@ -134,6 +148,10 @@ class ProxyServer(HTTPServer):
                         status_code = 404
                 proxy.fail()
                 tries -= 1
+                self.failures += 1
+            except:
+                print "Strange http bug."
+                proxy.fail()
                 self.failures += 1
             else:
                 self.successes += 1
@@ -148,21 +166,21 @@ class ProxyServer(HTTPServer):
                 if self.successes + self.failures > 0:
                     ratio = 100 * float(self.successes) / (self.successes + self.failures)
                     logging.info("%s / %s  - (%.2f%% success) - %s" % (
-                            self.successes,
-                            self.successes + self.failures,
-                            ratio,
-                            len(self.proxies)))
+                        self.successes,
+                        self.successes + self.failures,
+                        ratio,
+                        len(self.proxies)))
 
         if not success:
             # HTTP 417 is not to be confused with HTTP 418.
             msg = "Too many retries for %s." % request.uri
             request.write("HTTP/1.1 %s\r\nContent-Length: %d\r\n\r\n%s" %
-                    (status_code, len(msg), msg))
+                          (status_code, len(msg), msg))
             logging.info("%s failed" % request.uri)
         else:
             # logging.info("Success: %s" % request.uri)
             request.write("HTTP/1.1 200 OK\r\nContent-Length: %d\r\n\r\n%s" %
-                (len(response.body), response.body))
+                          (len(response.body), response.body))
         request.finish()
 
 
